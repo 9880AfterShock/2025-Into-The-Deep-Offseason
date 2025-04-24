9870be850a880929450b2a9b28e31e1b33ad1280
DaBiggestBozo <146139125+DaBiggestBozo@users.noreply.github.com>
2025-04-24 14:04:33 -0700

    added vision, why am i getting errors?

--- TeamCode/src/main/java/Mechanisms/Scoring/SpecimenSwivel.java
+++ TeamCode/src/main/java/Mechanisms/Scoring/SpecimenSwivel.java
@@ -3,54 +3,45 @@ package Mechanisms.Scoring;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import static java.lang.Math.PI;
+import static java.lang.Math.abs;
+import static java.lang.Math.atan2;
+
+import Mechanisms.Pickup.Wrist;
+
 public class SpecimenSwivel {
     private static Servo swivel;
-    public static double outPos = 1.0; // the positions
-    public static double inPos = 0.7; // the positions
-    public static boolean inited = false;
-    private static String state = "In";
-    private static boolean swivelButtonCurrentlyPressed = false;
-    private static boolean swivelButtonPreviouslyPressed = false;
+    private static double orientation = 0.5;
+    public static double restingState = 0.5;
 
     private static OpMode opmode;
 
     public static void initSwivel(OpMode opmode) {
-        swivel = opmode.hardwareMap.get(Servo.class, "Specimen Swivel"); // config name
+        swivel = opmode.hardwareMap.get(Servo.class, "Swivel");
+        orientation = 0.5;
+        restingState = 0.5;
         SpecimenSwivel.opmode = opmode;
-        state = "In";
-        inited = false;
     }
 
-    public static void moveOut() {
-        swivel.setPosition(outPos);
-        state = "Out";
-    }
-
-    private static void moveIn() {
-        swivel.setPosition(inPos); // swivel doesn't move
-        state = "In"; // this runs
-    }
-
-    private static void swap() {
-        if (state.equals("Out")) {
-            moveIn();
-        } else {
-            moveOut();
-        }
+    private static void moveTo(double orientation) {
+        swivel.setPosition(orientation);
     }
 
     public static void updateSwivel() {
-        opmode.telemetry.addData("Swivel Position", state);
-        // Check the status of the claw button on the gamepad
-        swivelButtonCurrentlyPressed = opmode.gamepad1.y; // change this to change the button // disabled for safety
+        if ((opmode.gamepad2.right_stick_y == 0.0 && opmode.gamepad2.right_stick_x == 0.0) || Wrist.currentPos == 2) {
+            orientation = restingState;
+        } else {
+            orientation = atan2(abs(opmode.gamepad2.right_stick_y), -opmode.gamepad2.right_stick_x);
+            orientation = abs(orientation / PI * (0.85 - 0.15) + 0.15); // boundaries are 0.85 and 0.15
 
-        // If the button state is different than what it was, then act
-        if (swivelButtonCurrentlyPressed != swivelButtonPreviouslyPressed) {
-            // If the button is (now) down
-            if (swivelButtonCurrentlyPressed) {
-                swap();
+            if (Wrist.currentPos == 0 /*&& Raiser.targPos != 0*/) { //might need to change for new mechs
+                restingState = 0.85; // for while it's down
+            } else {
+                restingState = 0.5;
             }
         }
-        swivelButtonPreviouslyPressed = swivelButtonCurrentlyPressed;
+        moveTo(orientation);
+
+        opmode.telemetry.addData("Claw Swivel Position", orientation);
     }
 }
