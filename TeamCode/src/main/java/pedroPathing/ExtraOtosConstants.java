a3eb14fb63295e765821c8bde7ba6a1a53681147
DaBiggestBozo <146139125+DaBiggestBozo@users.noreply.github.com>
2025-05-20 15:18:37 -0700

    tuning extra otos consts

--- TeamCode/src/main/java/pedroPathing/ExtraOtosConstants.java
+++ TeamCode/src/main/java/pedroPathing/ExtraOtosConstants.java
@@ -4,8 +4,8 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class ExtraOtosConstants {
-    public static  double X_MULTIPLIER = 1.36305263502669;
-    public static  double Y_MULTIPLIER = 1.5;
+    public static  double X_MULTIPLIER = 1.34;
+    public static  double Y_MULTIPLIER = 1.56;
     public static  double X_VEL_MULTIPLIER = 1.0;
     public static  double Y_VEL_MULTIPLIER = 1.0;
 
